d4c0d8a95b061c0b36517e4e93f5bc0d75ca6a27
Seyud <2159455958@qq.com>
2025-10-10 22:14:38 +0800

    build: 更新依赖版本至1.3.0

--- module/action.sh
+++ module/action.sh
@@ -2,15 +2,11 @@
 
 MODDIR=${0%/*}
 
-# 定义free文件路径
 FREE_FILE="$MODDIR/free"
 
-# 检查free文件是否存在
 if [ -f "$FREE_FILE" ]; then
-    # 读取当前内容
     CURRENT=$(cat "$FREE_FILE")
     
-    # 切换0和1
     if [ "$CURRENT" = "0" ]; then
         echo "1" > "$FREE_FILE"
         echo "已切换为PPS"
@@ -18,12 +14,10 @@ if [ -f "$FREE_FILE" ]; then
         echo "0" > "$FREE_FILE"
         echo "已切换为MIPPS"
     else
-        # 如果内容不是0或1，默认设置为1
         echo "1" > "$FREE_FILE"
         echo "内容无效，已重置为PPS"
     fi
 else
-    # 如果文件不存在，创建并设置为1
     echo "1" > "$FREE_FILE"
     echo "文件不存在，已创建并设置为PPS"
 fi
